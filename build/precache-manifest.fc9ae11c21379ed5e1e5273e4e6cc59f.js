2536cba958f7a54db862616246df199a14887cae
unrealandychan <unrealandychan@gmail.com>
2020-02-10 13:46:32 +0800

    Build

---
+++ build/precache-manifest.fc9ae11c21379ed5e1e5273e4e6cc59f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42945e0f8482ec592ec4ecfc2dc8d4bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fb36a5870c24fe18049",
+    "url": "/static/css/main.3590c28d.chunk.css"
+  },
+  {
+    "revision": "e0cc4cfbc2cf8eca8f05",
+    "url": "/static/js/2.26c12960.chunk.js"
+  },
+  {
+    "revision": "92c5f13aa40a7472c77041fa2a766ebc",
+    "url": "/static/js/2.26c12960.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fb36a5870c24fe18049",
+    "url": "/static/js/main.f4dada8a.chunk.js"
+  },
+  {
+    "revision": "7a98c973398fcbd01814",
+    "url": "/static/js/runtime-main.9dd3ea58.js"
+  }
+]);
